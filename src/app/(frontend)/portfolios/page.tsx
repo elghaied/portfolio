61b39fc30fe43aa566d4146e33d138ea9fce0401
elghaied <elghaied.eslam@gmail.com>
2024-09-03 03:13:59 +0200

    activating boxes

--- src/app/(frontend)/portfolios/page.tsx
+++ src/app/(frontend)/portfolios/page.tsx
@@ -101,7 +101,7 @@ export default async function Page() {
         <div className=" h-80 md:h-screen relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center ">
           <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
-          {/* <Boxes className="pointer-events-none md:pointer-events-auto" /> */}
+          <Boxes className="pointer-events-none md:pointer-events-auto" />
 
           <h1
             className={cn(
